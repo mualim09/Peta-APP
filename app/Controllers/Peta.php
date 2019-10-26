7acf82d5c72c1e8fd52370405695ac00ac071a96
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2019-10-26 17:33:35 +0700

    xml file, ok

--- app/Controllers/Peta.php
+++ app/Controllers/Peta.php
@@ -3,18 +3,12 @@
 namespace App\Controllers;
 use App\Models\PetaModel;
 use CodeIgniter\Controller;
-//use App\Libraries\DOMNode;
-//use App\Libraries\DOMDocument;
-//use App\Libraries\FluidXml\FluidXml;
-//require_once ROOTPATH . 'vendor/autoload.php';
-use \FluidXml\FluidXml;
-use \FluidXml\FluidNamespace;
 use function \FluidXml\fluidxml;
 use function \FluidXml\fluidns;
 use function \FluidXml\fluidify;
 class Peta extends BaseController
 {	
-	protected $domdoc;
+
 	public function index()
 	{
 		$model = new PetaModel();
@@ -64,53 +58,15 @@ class Peta extends BaseController
 
 	public function xml()
 	{
-		//$domClass = new \Libraries\DOMDocument();
 		$model = new PetaModel();
 		$listing = $model->getListing();
-		/*
-		$dom->encoding = 'utf-8';
-		$dom->xmlVersion = '1.0';
-		$dom->formatOutput = true;
-		$xml_file_name = 'peta_list.xml';
-
-		$root = $dom->createElement('markers');
+		$markers = fluidxml('markers');
 		foreach ($listing as $dataListing) {
-			$marker = $dom->createElement('marker');
-			$id = new DOMAttr('id', $dataListing['id']);
-			$name = new DOMAttr('name', $dataListing['name']);
-		
-		$marker_node->setAttributeNode($attr_movie_id);
-
-		$child_node_title = $dom->createElement('Title', 'The Campaign');
-
-		$movie_node->appendChild($child_node_title);
-
-		$child_node_year = $dom->createElement('Year', 2012);
-
-		$movie_node->appendChild($child_node_year);
-
-		$child_node_genre = $dom->createElement('Genre', 'The Campaign');
-
-		$movie_node->appendChild($child_node_genre);
-
-		$child_node_ratings = $dom->createElement('Ratings', 6.2);
-
-		$movie_node->appendChild($child_node_ratings);
-
-		$root->appendChild($marker);
-
-		$dom->appendChild($root);
+			$markers->add([ ['marker'] ], ['id' => $dataListing['id'], 'name' => $dataListing['name'], 'address' => $dataListing['address'], 'lat' => $dataListing['lat'], 'lng' => $dataListing['long'], 'type' => $dataListing['type'], 'description' => $dataListing['description'], 'image' => $dataListing['image'] ]);
 		}
-		$dom->save($xml_file_name);
-
-		echo "$xml_file_name has been successfully created";*/
 		
-		$book = fluidxml();
-		$book->add('title',  'The Theory Of Everything')
-     	->add('author', 'S. Hawking')
-     	->add('description')
-		->save('../xml/book2.xml', true);
-		echo $book;
-		 //echo getcwd();
+		
+		$markers->save('../xml/markers.xml', true);
+		echo $markers;
 	}
 }

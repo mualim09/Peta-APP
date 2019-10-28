ae0e19f5ab0ef0a87e95e9e8f73025939568d0eb
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2019-10-29 05:58:30 +0700

    Bootstrap untuk layout halaman utama

---
+++ app/Views/template/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head>

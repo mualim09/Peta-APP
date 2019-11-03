b248a0b3df6cca2ecbe9ff28b9714db64f4b8b2d
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2019-11-04 02:46:13 +0700

    Tambah bootstrap

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -3,5 +3,5 @@
 <head>
 	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>

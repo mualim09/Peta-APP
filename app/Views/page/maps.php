8f3ba322b73707dbd77b1a2fe4839d92fb83a147
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2019-11-04 09:46:37 +0700

    memindahkan bootstrap modal dari file konten ke maps, menggantikan infowindow menjadi bootstrap modal sesuai id

--- app/Views/page/maps.php
+++ app/Views/page/maps.php
@@ -18,6 +18,29 @@
       <div id="map"></div>
     </div>
   </div>
+
+  <div class="modal fade" id="modalDetail" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+          <h4 class="modal-title" id="modalDetailLabel">Detail</h4>
+        </div>
+        <div class="modal-body">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
   <script>
     var customLabel = {
       restoran: {
@@ -163,8 +186,16 @@
                 icon: icons[type].icon,
               });
             marker.addListener('click', function() {
-              infoWindow.setContent(infowincontent);
-              infoWindow.open(map, marker);
+              //infoWindow.setContent(infowincontent);
+              //infoWindow.open(map, marker);
+              $("#modalDetail").modal('show');
+                $.post(<?php echo "'".base_url("cari")."'";?>,
+                    {id:id},
+                    function(html){
+                        $(".modal-body").html(html);
+                        console.log(id);
+                    }
+                );
             });
           });
         });
@@ -193,6 +224,19 @@
   <script async defer
       src="https://maps.googleapis.com/maps/api/js?key=<?php echo $googleMapsApi; ?>&callback=initMap&libraries=places">
   </script>
-    <!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbn46MVj1kbVCtIzLbeGWIdxrwuogdOTo&libraries=places"></script> -->
-</body>
-</html>
+
+  <script>
+        $(function(){
+            $(document).on('click','.detail',function(e){
+                e.preventDefault();
+                $("#modalDetail").modal('show');
+                $.post(<?php echo "'".base_url("cari")."'";?>,
+                    {id:$(this).attr('data-id')},
+                    function(html){
+                        $(".modal-body").html(html);
+                        console.log(id);
+                    }
+                );
+            });
+        });
+    </script>

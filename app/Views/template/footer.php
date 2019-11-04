ea995d54c21722b09004735e16ab270aef8c7c16
Ahmad Bagwi <ahmadbagwi.id@gmail.com>
2019-11-04 09:46:51 +0700

    menyesuaikan footer

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,5 +1,3 @@
-	<script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
